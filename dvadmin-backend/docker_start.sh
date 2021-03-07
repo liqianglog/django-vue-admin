c31c62e21ceffb27a7972766943292ee5c94eca7
李强 <1206709430@qq.com>
2021-03-07 11:50:03 +0800

    docker 部署上传

--- dvadmin-backend/docker_start.sh
+++ dvadmin-backend/docker_start.sh
@@ -6,7 +6,7 @@ python ./manage.py makemigrations
 python ./manage.py migrate
 #python ./manage.py initialization
 #python ./manage.py runserver 0.0.0.0:8000
-uwsgi --ini azcrm/uwsgi.ini
+uwsgi --ini application/uwsgi.ini
 #if [[ $ENV == "preprod" ]] || [[ $ENV == "prod" ]]; then
 #    uwsgi --ini azcrm/uwsgi.ini
 #else
