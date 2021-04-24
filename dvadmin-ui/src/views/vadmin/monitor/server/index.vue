cd59054432a7e2574e4ac0b215f1cbc6d723e4da
xt12321 <1499410095@qq.com>
2021-04-24 15:59:22 +0800

    功能变化(服务监控): 清空记录成功时，改为使用自定义的提示信息；

--- dvadmin-ui/src/views/vadmin/monitor/server/index.vue
+++ dvadmin-ui/src/views/vadmin/monitor/server/index.vue
@@ -327,7 +327,7 @@ export default {
         cancelButtonText: '放弃'
       }).then(() => {
         cleanMonitorLog().then(results => {
-          this.msgSuccess(results.msg || '清除记录成功！')
+          this.msgSuccess( '清除记录成功！')
         }).catch(error => {
           this.$message.warning(error.msg || '清除记录失败，请重试！')
         })
