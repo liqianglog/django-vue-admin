9f52d633c16f27c76b54d579fbaf06466071356b
猿小天 <1638245306@qq.com>
2022-04-15 17:26:40 +0800

    功能变化: 修改mysql数据库配置的注释

--- backend/conf/env.example.py
+++ backend/conf/env.example.py
@@ -5,24 +5,25 @@ from application.settings import BASE_DIR
 # ================================================= #
 # ************** 数据库 配置  ************** #
 # ================================================= #
-#
+
 # 数据库 ENGINE ，默认演示使用 sqlite3 数据库，正式环境建议使用 mysql 数据库
 DATABASE_ENGINE = "django.db.backends.sqlite3"
 # 数据库名
 DATABASE_NAME = os.path.join(BASE_DIR, 'db.sqlite3')
 
+#************mysql配置*******************#
 # 使用mysql时，改为此配置
 # DATABASE_ENGINE = "django.db.backends.mysql"
 # DATABASE_NAME = 'django-vue-admin' # mysql 时使用
-
 # 数据库地址 改为自己数据库地址
-DATABASE_HOST = "127.0.0.1"
-# 数据库端口
-DATABASE_PORT = 3306
-# 数据库用户名
-DATABASE_USER = "root"
-# 数据库密码
-DATABASE_PASSWORD = "123456"
+# DATABASE_HOST = "127.0.0.1"
+# # 数据库端口
+# DATABASE_PORT = 3306
+# # 数据库用户名
+# DATABASE_USER = "root"
+# # 数据库密码
+# DATABASE_PASSWORD = "123456"
+
 # ================================================= #
 # ************** redis配置，无redis 可不进行配置  ************** #
 # ================================================= #
