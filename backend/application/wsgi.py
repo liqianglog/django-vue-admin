f1116659a05f0d13a292009def6c939f0797c00e
李强 <1206709430@qq.com>
2022-05-19 11:17:48 +0800

    功能变化: 修复daphne启用报错

--- backend/application/wsgi.py
+++ backend/application/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'application.settings')
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
 application = get_wsgi_application()
