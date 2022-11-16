149d8830c88293f435c7865b2258d215129352ed
李强 <1206709430@qq.com>
2022-11-17 00:05:24 +0800

    websocket 导入包bug

--- backend/application/asgi.py
+++ backend/application/asgi.py
@@ -14,8 +14,6 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'application.settings')
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
-from application.websocketConfig import websocket_application
-
 http_application = get_asgi_application()
 
 # async def application(scope,receive,send):
