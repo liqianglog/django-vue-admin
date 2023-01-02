a319030271b8d03602e8b638ae73c447fb60346b
猿小天 <1638245306@qq.com>
2023-01-02 16:39:37 +0800

    功能变化: 菜单按钮序列化器

--- backend/dvadmin/system/views/menu_button.py
+++ backend/dvadmin/system/views/menu_button.py
@@ -32,6 +32,16 @@ class MenuButtonInitSerializer(CustomModelSerializer):
         fields = ['id', 'name', 'value', 'api', 'method', 'menu']
         read_only_fields = ["id"]
 
+class MenuButtonCreateUpdateSerializer(CustomModelSerializer):
+    """
+    初始化菜单按钮-序列化器
+    """
+
+    class Meta:
+        model = MenuButton
+        fields = "__all__"
+        read_only_fields = ["id"]
+
 
 class MenuButtonViewSet(CustomModelViewSet):
     """
@@ -44,4 +54,6 @@ class MenuButtonViewSet(CustomModelViewSet):
     """
     queryset = MenuButton.objects.all()
     serializer_class = MenuButtonSerializer
+    create_serializer_class = MenuButtonCreateUpdateSerializer
+    update_serializer_class = MenuButtonCreateUpdateSerializer
     extra_filter_backends = []
