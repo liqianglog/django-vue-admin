3a02832e08b6aa2142ed9c2b42bf1ef0204214d2
猿小天 <1638245306@qq.com>
2023-01-12 22:02:38 +0800

    修复BUG: 新增导入和导入更新bug优化

--- backend/dvadmin/utils/import_export_mixin.py
+++ backend/dvadmin/utils/import_export_mixin.py
@@ -57,11 +57,6 @@ class ImportSerializerMixin:
             length += 2.1 if ord(char) > 256 else 1
         return round(length, 1) if length <= self.export_column_width else self.export_column_width
 
-    @action(methods=['get'],detail=False)
-    def update_field(self,request:Request):
-        data = [{"label":value,"value":key} for key,value in self.import_field_dict.items()]
-        return DetailResponse(data=data)
-
     @action(methods=['get','post'],detail=False)
     @transaction.atomic  # Django 事务,防止出错
     def import_data(self, request: Request, *args, **kwargs):
@@ -153,7 +148,7 @@ class ImportSerializerMixin:
             for ele in data:
                 filter_dic = {'id':ele.get('id')}
                 instance = filter_dic and queryset.filter(**filter_dic).first()
-                print(instance)
+                # print(156,ele)
                 serializer = self.import_serializer_class(instance, data=ele, request=request)
                 serializer.is_valid(raise_exception=True)
                 serializer.save()
@@ -170,15 +165,48 @@ class ImportSerializerMixin:
         response["Access-Control-Expose-Headers"] = f"Content-Disposition"
         response["content-disposition"] = f'attachment;filename={quote(str(f"导出{get_verbose_name(queryset)}.xlsx"))}'
         wb = Workbook()
+        ws1 = wb.create_sheet("data", 1)
+        ws1.sheet_state = "hidden"
         ws = wb.active
         import_field_dict = {}
-        for key,val in self.import_field_dict.items():
-            if isinstance(val,dict):
-                import_field_dict[key] = val.get('title')
+        header_data = ["序号","更新主键(勿改)"]
+        hidden_header = ["#","id"]
+        #----设置选项----
+        validation_data_dict = {}
+        for index, item in enumerate(self.import_field_dict.items()):
+            items = list(item)
+            key = items[0]
+            value = items[1]
+            if isinstance(value, dict):
+                header_data.append(value.get("title"))
+                hidden_header.append(value.get('display'))
+                choices = value.get("choices", {})
+                if choices.get("data"):
+                    data_list = []
+                    data_list.extend(choices.get("data").keys())
+                    validation_data_dict[value.get("title")] = data_list
+                elif choices.get("queryset") and choices.get("values_name"):
+                    data_list = choices.get("queryset").values_list(choices.get("values_name"), flat=True)
+                    validation_data_dict[value.get("title")] = list(data_list)
+                else:
+                    continue
+                column_letter = get_column_letter(len(validation_data_dict))
+                dv = DataValidation(
+                    type="list",
+                    formula1=f"{quote_sheetname('data')}!${column_letter}$2:${column_letter}${len(validation_data_dict[value.get('title')]) + 1}",
+                    allow_blank=True,
+                )
+                ws.add_data_validation(dv)
+                dv.add(f"{get_column_letter(index + 3)}2:{get_column_letter(index + 3)}1048576")
             else:
-                import_field_dict[key] = val
-        header_data = ["序号","更新主键(勿改)", *import_field_dict.values()]
-        hidden_header = ["#","id", *import_field_dict.keys()]
+                header_data.append(value)
+                hidden_header.append(key)
+        # 添加数据列
+        ws1.append(list(validation_data_dict.keys()))
+        for index, validation_data in enumerate(validation_data_dict.values()):
+            for inx, ele in enumerate(validation_data):
+                ws1[f"{get_column_letter(index + 1)}{inx + 2}"] = ele
+        #--------
         df_len_max = [self.get_string_len(ele) for ele in header_data]
         row = get_column_letter(len(hidden_header) + 1)
         column = 1
