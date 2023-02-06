d928ddee20ba13fcd23f12d78c44969c305b8359
H0nGzA1 <2505811377@qq.com>
2023-02-06 16:21:32 +0800

    feat(完成前端菜单路由): ✨ 菜单icon，i18适配

--- web/src/utils/setIconfont.ts
+++ web/src/utils/setIconfont.ts
@@ -1,6 +1,7 @@
 // 字体图标 url
 const cssCdnUrlList: Array<string> = [
 	'//at.alicdn.com/t/font_2298093_y6u00apwst.css',
+	'//at.alicdn.com/t/c/font_3882322_8vb7gh5lw4t.css', //dvadmin3项目用icon
 	'//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
 ];
 // 第三方 js url
