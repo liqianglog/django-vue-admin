a06412d15dd564d49917729fd8ccfc3efa06c7fc
李强 <1206709430@qq.com>
2023-02-06 10:13:07 +0800

    新功能: 优化

--- backend/kfm_apps/carton_manage/production_manage/views/production_work_status_record.py
+++ backend/kfm_apps/carton_manage/production_manage/views/production_work_status_record.py
@@ -12,8 +12,8 @@ class ProductionWorkStatusRecordSerializer(CustomModelSerializer):
     """
     生产状态记录-序列化器
     """
-    production_work_no = serializers.CharField(source="production_work.no",read_only=True)
-    code_type = serializers.IntegerField(source="production_work.code_package.code_type",read_only=True)
+    production_work_no = serializers.CharField(source="production_work.no", read_only=True)
+    code_type = serializers.IntegerField(source="production_work.code_package.code_type", read_only=True)
     order_id = serializers.CharField(source="production_work.order_id", read_only=True)
     factory_info_name = serializers.CharField(source="production_work.factory_info.name", read_only=True)
     production_line_name = serializers.CharField(source="production_work.production_line.name", read_only=True)
@@ -48,15 +48,18 @@ class ProductionWorkStatusRecordUpdateSerializer(CustomModelSerializer):
 
 class ProductionWorkStatusRecordFilterSet(FilterSet):
     production_work = django_filters.CharFilter(field_name="production_work__id", lookup_expr="icontains")
-    code_type = django_filters.NumberFilter(field_name="production_work__code_package__code_type", lookup_expr="icontains")
-    factory_info_name = django_filters.CharFilter(field_name="production_work__factory_info__name", lookup_expr="icontains")
-    production_line_name = django_filters.CharFilter(field_name="production_work__production_line__name", lookup_expr="icontains")
+    code_type = django_filters.NumberFilter(field_name="production_work__code_package__code_type",
+                                            lookup_expr="icontains")
+    factory_info_name = django_filters.CharFilter(field_name="production_work__factory_info__name",
+                                                  lookup_expr="icontains")
+    production_line_name = django_filters.CharFilter(field_name="production_work__production_line__name",
+                                                     lookup_expr="icontains")
     device_name = django_filters.CharFilter(field_name="production_work__device__name", lookup_expr="icontains")
 
-
-class Meta:
+    class Meta:
         model = ProductionWorkStatusRecord
         fields = '__all__'
+
 
 class ProductionWorkStatusRecordViewSet(CustomModelViewSet):
     """
