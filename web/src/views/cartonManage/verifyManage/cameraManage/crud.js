d11d61b4683a786716f4c10b6ff997692cfbc5fb
猿小天 <1638245306@qq.com>
2023-02-07 15:24:10 +0800

    新功能: 相机管理页面

---
+++ web/src/views/cartonManage/verifyManage/cameraManage/crud.js
@@ -0,0 +1,111 @@
+import { BUTTON_STATUS_NUMBER } from '@/config/button'
+import { request } from '@/api/service'
+import { urlPrefix as factoryInfoUrlPrefix } from '../factoryInfo/api'
+import util from '@/libs/util'
+
+export const crudOptions = (vm) => {
+  return {
+    pageOptions: {
+      compact: true
+    },
+    options: {
+      tableType: 'vxe-table',
+      rowKey: true, // 必须设置，true or false
+      rowId: 'id',
+      height: '100%', // 表格高度100%, 使用toolbar必须设置
+      highlightCurrentRow: false
+
+    },
+    rowHandle: {
+      view: {
+        thin: true,
+        text: '',
+        disabled () {
+          return !vm.hasPermissions('Retrieve')
+        }
+      },
+      width: 140,
+      edit: false,
+      remove: false
+    },
+    viewOptions: {
+      componentType: 'row'
+    },
+    formOptions: {
+      defaultSpan: 12 // 默认的表单 span
+    },
+    indexRow: { // 或者直接传true,不显示title，不居中
+      title: '序号',
+      align: 'center',
+      width: 80
+    },
+    columns: [
+      {
+        title: '关键词',
+        key: 'search',
+        show: false,
+        disabled: true,
+        search: {
+          disabled: false
+        },
+        form: {
+          disabled: true,
+          component: {
+            placeholder: '请输入关键词'
+          }
+        },
+        view: {
+          disabled: true
+        }
+      }, {
+        title: 'ID',
+        key: 'id',
+        show: false,
+        form: {
+          disabled: true
+        }
+      }, {
+        title: '相机编号',
+        key: 'no',
+        width: 120,
+        search: {
+          disabled: false
+        },
+        type: 'input',
+        form: {
+          disabled: true
+        }
+      },
+      {
+        title: '设备名称',
+        key: 'device_name',
+        search: {
+          disabled: false
+        },
+        form: {
+          disabled: true
+        }
+      },
+      {
+        title: '工厂名称',
+        key: 'factory_name',
+        search: {
+          disabled: false
+        },
+        form: {
+          disabled: true
+        }
+      },
+      {
+        title: '产线名称',
+        key: 'prod_line_name',
+        search: {
+          disabled: false
+        },
+        form: {
+          disabled: true
+        }
+      }
+    ]
+  }
+}
