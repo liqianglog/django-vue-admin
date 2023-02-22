ba204478c717ba05684ee34ef0383708e1962c3b
猿小天 <1638245306@qq.com>
2023-02-22 16:19:03 +0800

    功能变化: 重构初始化功能

--- backend/dvadmin/system/fixtures/initSerializer.py
+++ backend/dvadmin/system/fixtures/initSerializer.py
@@ -144,7 +144,7 @@ class RoleMenuInitSerializer(CustomModelSerializer):
 
     def create(self, validated_data):
         init_data = self.initial_data
-        validated_data['menu_component_name'].pop()
+        validated_data.pop('menu_component_name')
         role_id = Role.objects.filter(key=init_data['role_key']).first()
         menu_id = Menu.objects.filter(component_name=init_data['menu_component_name']).first()
         validated_data['role'] = role_id
