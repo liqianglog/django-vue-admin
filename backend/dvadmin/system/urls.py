ace9c005318a4c8db6668b5e956ba66c59903b3b
猿小天 <1638245306@qq.com>
2023-02-26 21:00:46 +0800

    功能变化: bug修复

--- backend/dvadmin/system/urls.py
+++ backend/dvadmin/system/urls.py
@@ -30,8 +30,6 @@ system_url.register(r'system_config', SystemConfigViewSet)
 system_url.register(r'message_center',MessageCenterViewSet)
 
 urlpatterns = [
-    path('user/export/', UserViewSet.as_view({'post': 'export_data', })),
-    path('user/import/', UserViewSet.as_view({'get': 'import_data', 'post': 'import_data'})),
     path('system_config/save_content/', SystemConfigViewSet.as_view({'put': 'save_content'})),
     path('system_config/get_association_table/', SystemConfigViewSet.as_view({'get': 'get_association_table'})),
     path('system_config/get_table_data/<int:pk>/', SystemConfigViewSet.as_view({'get': 'get_table_data'})),
