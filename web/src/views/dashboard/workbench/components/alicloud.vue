aebbe11b7006f82b5f3e2a1d8ab68cbf652e8dfb
raymond531 <victoryraymond@vip.qq.com>
2023-03-30 18:17:41 +0800

    feat: 新增三个主页卡片

---
+++ web/src/views/dashboard/workbench/components/alicloud.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-card shadow="never" header="阿里云" class="item-background">
+    <img src="/image/card/ali.png" @click="Jump()" alt="阿里云" class="img-style">
+  </el-card>
+</template>
+
+<script>
+export default {
+  title: '阿里云推广',
+  icon: 'el-icon-present',
+  description: '阿里云推广',
+  height: 15,
+  minH: 10,
+  width: 14,
+  minW: 10,
+  isResizable: true,
+  methods: {
+    Jump () {
+      window.open('https://www.aliyun.com/minisite/goods?userCode=jpef8a71&share_source=copy_link', '_black')
+    }
+  }
+}
+</script>
---
+++ web/src/views/dashboard/workbench/components/sms.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-card shadow="never" header="短信服务" class="item-background">
+    <img src="/image/card/sms.png" @click="Jump()" alt="短信服务" class="img-style">
+  </el-card>
+</template>
+
+<script>
+export default {
+  title: '短信服务',
+  icon: 'el-icon-message',
+  description: '点击跳转到短信服务',
+  height: 10,
+  minH: 10,
+  width: 5,
+  minW: 5,
+  isResizable: true,
+  methods: {
+    Jump () {
+      window.open('https://bbs.django-vue-admin.com/plugMarket/128.html', '_black')
+    }
+  }
+}
+</script>
