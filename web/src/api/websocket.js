0228a0b927528c2bb2f1193eb8efcb4f82c75018
猿小天 <1638245306@qq.com>
2023-04-08 22:24:01 +0800

    功能变化: 修改系统配置后,进行WS通知

--- web/src/api/websocket.js
+++ web/src/api/websocket.js
@@ -29,9 +29,14 @@ function webSocketOnError (e) {
  */
 function webSocketOnMessage (e) {
   const data = JSON.parse(e.data)
-  const { refresh_unread } = data
+  const { refresh_unread, systemConfig } = data
   if (refresh_unread) {
+    // 更新消息通知条数
     store.dispatch('d2admin/messagecenter/setUnread')
+  }
+  if (systemConfig) {
+    // 更新系统配置
+    this.$store.dispatch('d2admin/settings/load')
   }
   if (data.contentType === 'SYSTEM') {
     ElementUI.Notification({
