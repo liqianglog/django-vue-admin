57f12cc92eefe756d0ca0aaadaef1a95d0b8cf43
李强 <1206709430@qq.com>
2023-04-12 14:59:27 +0800

    新功能: model 更新

--- backend/kfm_apps/basics_manage/models.py
+++ backend/kfm_apps/basics_manage/models.py
@@ -198,7 +198,7 @@ class JetPrintTemplate(CoreModel):
 
 
 class JetPrintTemplateAttribute(CoreModel):
-    number = models.IntegerField(default=0, blank=True, help_text="字段序号", verbose_name="字段序号")
+    number = models.IntegerField(default=1, blank=True, help_text="字段序号", verbose_name="字段序号")
     name = models.CharField(max_length=50, null=True, blank=True, help_text="字段名称", verbose_name="字段名称")
     code_package_template = models.ForeignKey(JetPrintTemplate, db_constraint=False, on_delete=models.PROTECT,
                                               help_text="所属喷印模板", verbose_name="所属喷印模板")
