b2aa22ce84c4abae55822dc1346b15c0c1565cf6
ahhui <ahhuiguys@163.com>
2023-08-03 18:04:34 +0800

    refactor:  重构配置文件
    所有项目的app都放在env.py里的CUSTOM_APPS列表里注册，为了支持按照项目app查找model

---
+++ backend/conf/env.example.py
@@ -0,0 +1,10 @@
+DATABASE_ENGINE = 'django.db.backends.mysql'
+DATABASE_NAME = 'dvadmin3'
+DATABASE_USER = 'root'
+DATABASE_PASSWORD = 'root'
+DATABASE_HOST = '127.0.0.1'
+DATABASE_PORT = '3306'
+ALLOWED_HOSTS = ['*']
+CUSTOM_APPS = [
+    "dvadmin.system",
+]
