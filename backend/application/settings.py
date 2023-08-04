19df23ce5f8cff80df651d040f6a351c9ce7a39d
ahhui <ahhuiguys@163.com>
2023-08-04 18:12:58 +0800

    chore:  更改jwt有效期

--- backend/application/settings.py
+++ backend/application/settings.py
@@ -306,7 +306,7 @@ AUTHENTICATION_BACKENDS = ["dvadmin.utils.backends.CustomBackend"]
 # ================================================= #
 SIMPLE_JWT = {
     # token有效时长
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1440),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120),
     # token刷新后的有效时间
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     # 设置前缀
