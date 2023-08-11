65af2407573c56c6be36884bac0a1c0b10fe2434
符锦辉 <ahhuiguys@163.com>
2023-08-11 17:41:25 +0800

    删除 device

--- backend/device/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from device.websocket import DeviceStatusWebSocket
-
-websocket_urlpatterns = [
-    path('ws/gateway_status/<str:token>/', DeviceStatusWebSocket.as_asgi()),
-]
