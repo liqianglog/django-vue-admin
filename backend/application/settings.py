e4432ea920c2711e807d8daf8e2c9f3e1aef4442
李强 <1206709430@qq.com>
2023-08-21 10:27:57 +0800

    功能变化: 新增存活检测功能

--- backend/application/settings.py
+++ backend/application/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "dvadmin.utils.middleware.HealthCheckMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
